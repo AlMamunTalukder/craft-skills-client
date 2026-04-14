de0c3457feba9b165f2480997cf917ae3bb04d98
AlMamunTalukder <almamuntalukder0@gmail.com>
2026-04-14 09:35:26 +0600

    update code for push

--- src/utils/dataLayer.ts
+++ src/utils/dataLayer.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 export function pushEvent(eventName: string, params: Record<string, any>) {
   if (typeof window === "undefined") return;
 
   window.dataLayer = window.dataLayer || [];
 
-  // 🔥 clear previous ecommerce data
   window.dataLayer.push({ ecommerce: null });
 
   window.dataLayer.push({
